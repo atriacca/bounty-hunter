a12a638ed0c49e6f0461cf24115315ccd209e31a
Alan Triacca <atriacca@yahoo.com>
2019-05-08 14:29:21 -0600

    commit for Heroku deployment

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const path = require('path') 
 const PORT = process.env.PORT || 7000
 
 // Middlewares for every request Naga Sadow
 app.use(express.json()) // req.body = Object from POST and PUT requests
 app.use(morgan('dev'))
+// For Heroku deployment (VERY important):
+app.use(express.static(path.join(_dirname, "client", "build")))
 
 // Connect to mongoDB
-mongoose.connect("mongodb://localhost:27017/bounties", {useNewUrlParser: true}, () => {
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost:27017/bounties", {useNewUrlParser: true}, () => {
     console.log("connected to the DB")
 })
 
@@ -20,6 +25,11 @@ app.use("/bounties", require('./routes/bountyRouter.js'))
 app.use((err, req, res, next) => {
     console.error(err)
     return res.send({errMsg: err.message})
+})
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 
 app.listen(PORT, () => {
